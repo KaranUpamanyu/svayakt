5b1c205e52236239f2b5ab4aeb3a00192ca4fc1e
Karan Venkatesh Upamanyu <karan.v.upamanyu@gmail.com>
2022-06-17 21:39:57 +0530

    commit

---
+++ components/status.js
@@ -0,0 +1,59 @@
+import styles from '../styles/Home.module.css'
+import {GraphQLClient, gql} from 'graphql-request'
+
+function Status() {
+  const ENDPOINT = `https://api-ap-south-1.graphcms.com/v2/cl4doen4r64qw01xu8aht6kqs/master`
+  const graphcms = new GraphQLClient(ENDPOINT)
+  var today = (new Date()).getDate()
+
+  const QUERY = gql`query {
+    dailyRecords(where: {date: "2011-01-13"}) {
+      n
+      p
+      k
+      humidity
+      ph
+      rainfall
+    }
+  }`
+
+  graphcms.request(QUERY)
+  .then((data) => {
+    console.log(data.humidity)
+    return (
+      <a href="#" className={styles.card}>
+        <h2>Status &rarr;</h2>
+        <p>N: {data.n}</p>
+        <p>P: {data.p}</p>
+        <p>K: {data.k}</p>
+        <p>Humidity: {data.humidity}</p>
+        <p>pH: {data.ph}</p>
+        <p>Rainfall: {data.rainfall}</p>
+      </a>
+    );
+  })
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <a href="#" className={styles.card}>
+      <h2>Status &rarr;</h2>
+      <p>N: 93</p>
+      <p>P: 56</p>
+      <p>K: 36</p>
+      <p>Humidity: 82.06</p>
+      <p>pH: 6.98</p>
+      <p>Rainfall: 185.28</p>
+    </a>
+  );
+}
+
+export default Status;
---
+++ components/weather.js
@@ -0,0 +1,225 @@
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+// import request from 'superagent'
+import axios from 'axios'
+const https = require("https");
+
+function Weather() {
+  const appid = "9a93d45262919088181c7d3631049c02";
+  var locn = 'bangalore';
+  var units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+locn+"&appid="+appid+"&units="+units;
+
+  function getData() {
+    return new Promise((resolve, reject) => {
+      https.get(url,function(response){
+        console.log(response.statusCode);
+        response.on("data",function(data){
+          try {
+            parsedData = JSON.parse(data);
+            console.log(`log1: ${JSON.stringify(parsedData.main)} and ${JSON.stringify(parsedData.weather[0].desc)}`);
+            resolve(parsedData)
+          } catch (e) {
+            reject(e.message);
+          }
+        });
+      }).on('error', (e) => {
+        reject(`Got error: ${e.message}`);
+      });
+    })
+  }
+
+
+  getData()
+  .then(response => {
+    temperature = response.main.temp;
+    desc = response.weather[0].description;
+    icon = response.weather[0].icon;
+    console.log(`temp: ${temperature} and desc: ${desc}`);
+    iconurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+    return (
+      <a href="#" className={styles.card}>
+        <h2>Today's Weather &rarr;</h2>
+        <p>Temperature: {temperature}°C</p>
+        <p>Condition: {desc}</p>
+        <Image layout="fill" loader={() => iconurl} src={iconurl} alt="Weather Icon" />
+      </a>
+    );
+  })
+  .catch(error => {
+    return (
+      <a href="#" className={styles.card}>
+        <h2>Today's Weather &rarr;</h2>
+        <p>{error.message}</p>
+      </a>
+    );
+  });
+
+
+
+
+
+
+  // https.get(url,function(response){
+  //   console.log(response.statusCode);
+
+  //   response.on("data",function(data){
+  //     data = JSON.parse(data);
+  //     // console.log(JSON.stringify(data.main));
+  //     temperature = data.main.temp;
+  //     desc = data.weather[0].description;
+  //     icon = data.weather[0].icon;
+  //     iconurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+  //   });
+  // });
+
+
+  var iconurl = "http://openweathermap.org/img/wn/02d@2x.png";
+
+  return (
+    <a href="#" className={styles.card}>
+      <h2>Today's Weather &rarr;</h2>
+      <p>Temperature: 26.3°C</p>
+      <p>Condition: few clouds</p>
+      <img src={iconurl} alt='few clouds'/>
+      {/* <Image loader={() => iconurl} src={iconurl} alt="Weather Icon" /> */}
+    </a>
+  );
+
+
+
+
+  // axios.get(url)
+  // .then((data) => {
+  //   // console.log(JSON.stringify(data));
+  //   return (
+  //     <a
+  //       href="https://github.com/vercel/next.js/tree/canary/examples"
+  //       className={styles.card}
+  //     >
+  //       <h2>It be &rarr;</h2>
+  //       <p>like that sometimes.</p>
+  //       <p>{JSON.stringify(data.main)}</p>
+  //     </a>
+  //   )
+  // })
+  // .catch((error) => {
+  //   console.log(error.message);
+  //   return (
+  //     <a
+  //       href="https://github.com/vercel/next.js/tree/canary/examples"
+  //       className={styles.card}
+  //     >
+  //       <h2>It be &rarr;</h2>
+  //       <p>like that sometimes.</p>
+  //       <p>{error.message}</p>
+  //     </a>
+  //   )
+  // })
+
+  // async function getData () {
+  //   return await axios.get(url);
+  // }
+
+
+  // getData().then(data => {
+  //   return  (
+  //     <a
+  //       href="https://github.com/vercel/next.js/tree/canary/examples"
+  //       className={styles.card}
+  //     >
+  //       <h2>It be &rarr;</h2>
+  //       <p>like that sometimes.</p>
+  //       <p>{JSON.stringify(data)}</p>
+  //     </a>
+  //   )
+  // })
+
+
+
+
+
+
+  // try {
+    // https.get(url,function(response){
+    //   // console.log(response.statusCode);
+
+    //   response.on("data",function(data){
+    //     data = JSON.parse(data);
+    //     var temperature = data.main.temp;
+    //     var desc = data.weather[0].description;
+    //     var icon = data.weather[0].icon;
+    //     var iconurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+
+        // return (
+        //   <a href="#" className={styles.card}>
+        //     <h2>Today's Weather &rarr;</h2>
+        //     <p>Temperature: {temperature}°C</p>
+        //     <p>Condition: {desc}</p>
+        //     <Image layout="fill" loader={() => iconurl} src={iconurl} alt="Weather Icon" />
+        //   </a>
+        // );
+    //   });
+    // });
+
+    // fetch(url)
+    // .then(data => {
+    //   // data = JSON.parse(data);
+    //   console.log(data.toString());
+    //   var temperature = data.main.temp;
+    //   var desc = data.weather[0].description;
+    //   var icon = data.weather[0].icon;
+    //   var iconurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+
+    //   return (
+    //     <a href="#" className={styles.card}>
+    //       <h2>Today's Weather &rarr;</h2>
+    //       <p>Temperature: {temperature}°C</p>
+    //       <p>Condition: {desc}</p>
+    //       <Image layout="fill" loader={() => iconurl} src={iconurl} alt="Weather Icon" />
+    //     </a>
+    //   );
+    // })
+
+
+    // request
+    // .get(url)
+    // .then(data => {
+    //   // data = JSON.parse(data);
+    //   console.log(JSON.stringify(data.main.temp));
+    //   var temperature = data.main.temp;
+    //   var desc = data.weather[0].description;
+    //   var icon = data.weather[0].icon;
+    //   var iconurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+
+    //   return (
+    //     <a href="#" className={styles.card}>
+    //       <h2>Today's Weather &rarr;</h2>
+    //       <p>Temperature: {temperature}°C</p>
+    //       <p>Condition: {desc}</p>
+    //       <Image layout="fill" loader={() => iconurl} src={iconurl} alt="Weather Icon" />
+    //     </a>
+    //   );
+    // })
+    // .catch(err => {
+    //   return (
+    //     <a href="#" className={styles.card}>
+    //       <h2>Weather &rarr;</h2>
+    //       <p>Sorry buddy but this isn't your day because</p>
+    //       <p>{err.message}</p>
+    //     </a>
+    //   );
+    // });
+  // }
+  // catch(err) {
+  //   return (
+  //     <a href="#" className={styles.card}>
+  //       <h2>Weather &rarr;</h2>
+  //       <p>Sorry buddy but this isn't your day because</p>
+  //       <p>{err.message}</p>
+  //     </a>
+  //   );
+  // }
+}
+
+export default Weather;
